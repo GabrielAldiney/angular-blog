66192bba018ea1539b07ccea36f9918cb153b195
Gabriel Aldiney <gabrielaldiney@gmail.com>
2023-10-20 05:49:35 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,13 @@
+export const dataFake = [{
+  "id":"1",
+  "title":"Saiu a nova versão do Angular",
+  "description": "A equipe do Google anuncia uma nova versão do Angular",
+  "photoCover":"https://i.ytimg.com/vi/2vA4zfyLqh4/maxresdefault.jpg"
+},
+{
+"id":"2",
+"title":"Santander oferece bolsas para Bootcamps gratuitas",
+"description": "olá",
+"photoCover":"https://pro-becas-images-s3.s3.eu-west-1.amazonaws.com/public_documents/c642b5d5-7b94-4d15-a0c3-a762aeddf3ad"
+}
+]
